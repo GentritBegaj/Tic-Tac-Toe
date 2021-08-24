d4f038cf14e9b59e096855346556f37ce3272d71
GentritBegaj <gentritbegaj@gmail.com>
2021-08-24 17:09:25 +0200

    fix

---
+++ components/Square.tsx
@@ -0,0 +1,24 @@
+import { Player } from '../types';
+
+const Square = ({
+  value,
+  onClick,
+  winner,
+}: {
+  winner: Player;
+  value: Player;
+  onClick: () => void;
+}) => {
+  if (!value) {
+    return (
+      <button className="square" onClick={onClick} disabled={Boolean(winner)} />
+    );
+  }
+  return (
+    <button className={`square square_${value.toLocaleLowerCase()}`} disabled>
+      {value}
+    </button>
+  );
+};
+
+export default Square;
